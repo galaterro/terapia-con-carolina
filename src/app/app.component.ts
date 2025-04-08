57b79c40c04f693c0847039305e1ba0ff9831eb7
Galaterro <adrian.delagala@proton.me>
2025-04-08 23:41:54 +0200

    Add AboutMe, Valores, Creatividad, Formacion, and Localizacion components to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroComponentComponent } from './components/hero-component/hero-component.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ValoresComponent } from "./components/valores/valores.component";
+import { CreatividadComponent } from "./components/creatividad/creatividad.component";
+import { FormacionComponent } from './components/formacion/formacion.component';
+import { LocalizacionComponent } from './components/localizacion/localizacion.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeroComponentComponent],
+  imports: [RouterOutlet, HeroComponentComponent, AboutMeComponent, ValoresComponent, CreatividadComponent, FormacionComponent, LocalizacionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
