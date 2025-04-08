26451bef7adb6882762787f0c871af0f364a4371
Galaterro <adrian.delagala@proton.me>
2025-04-08 23:41:37 +0200

    Add Valores component with HTML, CSS, and TypeScript files

---
+++ src/app/components/valores/valores.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-valores',
+  templateUrl: './valores.component.html',
+  styleUrls: ['./valores.component.css']
+})
+export class ValoresComponent {
+}
