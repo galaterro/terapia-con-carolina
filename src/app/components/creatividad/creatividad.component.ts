554713c80b767b55e637922e3f29ea2d25173de4
Galaterro <adrian.delagala@proton.me>
2025-04-08 23:41:03 +0200

    Add Creatividad component with HTML, CSS, and TypeScript files

---
+++ src/app/components/creatividad/creatividad.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-creatividad',
+  imports: [],
+  templateUrl: './creatividad.component.html',
+  styleUrl: './creatividad.component.css'
+})
+export class CreatividadComponent {
+
+}
