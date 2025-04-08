0a833a8544fed8a7cd30e13f14f401c6d58fad7d
Galaterro <adrian.delagala@proton.me>
2025-04-08 23:41:27 +0200

    Add Localizacion component with HTML, CSS, and unit tests

---
+++ src/app/components/localizacion/localizacion.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-localizacion',
+  imports: [],
+  templateUrl: './localizacion.component.html',
+  styleUrl: './localizacion.component.css'
+})
+export class LocalizacionComponent {
+
+}
